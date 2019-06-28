a55a678443bc85a4a7c451b3777eeee853c82c00
jdm1219 <wjsehaud9413@naver.com>
2019-06-28 23:16:44 +0900

    edit ddrawer

--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -1,35 +1,42 @@
 <template>
-    <v-navigation-drawer
-    fixed
-    >
-        <v-toolbar flat>
-        <v-list>
-            <v-list-tile>
-            <v-list-tile-title class="title">
-                Navigation
-            </v-list-tile-title>
-            </v-list-tile>
-        </v-list>
+    <nav>
+        <v-toolbar flat app fixed>
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
         </v-toolbar>
-
-        <v-divider></v-divider>
-
-        <v-list dense class="pt-0">
-        <v-list-tile
-            v-for="item in items"
-            :key="item.title"
-            :to="item.link"
+        <v-navigation-drawer
+        v-model="drawer"
+        app
+        fixed
         >
-            <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-        </v-list-tile>
-        
-        </v-list>
-    </v-navigation-drawer>
+            <v-toolbar flat>
+            <v-list>
+                <v-list-tile>
+                <v-list-tile-title class="title">
+                    Navigation
+                </v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+            </v-toolbar>
+
+            <v-divider></v-divider>
+
+            <v-list dense class="pt-0">
+            <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+                :to="item.link"
+            >
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
 </template>
 
 <script>
@@ -40,11 +47,14 @@ export default {
                 {title: 'Dashboard', icon: 'dashboard', link: '/'},
                 { title: 'Chat', icon: 'widgets', link: 'chat'}
             ],
-            years: [
-
-            ],
-            right: null
+            drawer: true
         }
     } 
 }
 </script>
+
+<style>
+    .v-toolbar {
+        padding-left: 0 !important;
+    }
+</style>
